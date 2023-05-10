5a7bc20d7804c280750989db9d9f9f14c279c38c
elpitero <pio_dud@wp.pl>
2023-05-10 14:06:09 +0200

    Base configuration

---
+++ backend/src/test/java/com/pwr/ztw/ZtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwr.ztw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
