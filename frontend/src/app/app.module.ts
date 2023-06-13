d172bf1fd806dcb648dce690982472eb06a17886
wgruba <wgruba08@gmail.com>
2023-06-14 00:09:10 +0200

    finish front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,19 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConnectionLookupComponent } from './connection-lookup/connection-lookup.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QRCodeModule } from 'angularx-qrcode';
 
 
-import { PageConnectionComponent } from './page-connection/page-connection.component';
 import { CourseSelectionComponent } from './course-selection/course-selection.component';
-import { SeatSelectionComponent } from './seat-selection/seat-selection.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeSiteComponent } from './home-site/home-site.component';
@@ -38,15 +34,11 @@ import { GoogleMapsModule } from '@angular/google-maps'
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectionLookupComponent,
-    PageConnectionComponent,
     CourseSelectionComponent,
-    SeatSelectionComponent,
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
     MainComponent,
-    SidebarComponent,
     HomeSiteComponent,
     SearchedSiteComponent,
     SearchedContentComponent,
--- frontend/src/app/cart-content/cart-content.component.ts
+++ frontend/src/app/cart-content/cart-content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-cart-content',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-content.component.css']
 })
 export class CartContentComponent {
+
+  constructor(private router: Router) { }
+  
+  confirmPurchase(): void {
+    this.router.navigate(['/']);
+  }
 
 }
